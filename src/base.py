0cf78cbae94130f3e17b77bca6676cbf7518cd7a
rudoff94 <rudovd94@gmail.com>
2020-03-29 16:48:38 +0000

    Fixed base.py and added file for testing

--- src/base.py
+++ src/base.py
@@ -1,24 +1,36 @@
+#!/usr/bin/env python
 import re
 import os
 import serial
 from constants import ArduinoCommand
 import rospy
-
+from rpi_platform_ros.msg import BaseMotorControl
+from time import sleep
 class Base:
+
 	def __init__(self):
-		rospy.init('base_node')
+		rospy.init_node('base_node')
 		self.arduino = None
 		self.speed = 9600
 		rospy.on_shutdown(self.close)
 		#add topic - control base
+		self.motor_data_sub = rospy.Subscriber("base_motor_control", BaseMotorControl, self.handler_motor_data)
+	
+	def handler_motor_data(self, motor_data):
+		print(motor_data)
+		self.set_motor(motor_data.motor, motor_data.direction, motor_data.power)
 
-	def loop():
-		while True:
+
+	def loop(self):
+		while not rospy.is_shutdown():
 			while not rospy.is_shutdown():
 				if self.find_arduino():
 					break
+				sleep(1)
 			while not rospy.is_shutdown():
-				pass
+				if self.arduino == None:
+					break
+				sleep(1)
 
 	def find_arduino(self):
 		dev_list = os.listdir('/dev')
@@ -26,7 +38,7 @@ class Base:
 
 		for cur_dev in dev_list:
 			if re.match(patern, cur_dev):
-				print("Device: ", cur_dev)
+				print("Connected to device: ", cur_dev)
 				self.arduino = serial.Serial('/dev/' + cur_dev, self.speed)
 				return True
 		self.arduino = None
@@ -34,20 +46,16 @@ class Base:
 		return False
 
 	def set_motor(self, motor, dir, power):
-		if self.arduino is not None:
-			if (motor >= ArduinoCommand.MOTOR_L and motor <= ArduinoCommand.MOTOR_R):
-				if (dir >= ArduinoCommand.BACKWARD and dir <= ArduinoCommand.FORWARD):
-					if (power >= ArduinoCommand.MINPOWER and power <= ArduinoCommand.MAXPOWER): 
-						#Проблема была в кодировке
-						#Для того, чтобы перевести число в байт: bytes([число])
-						message = [bytes([motor]), bytes([dir]), bytes([power])]
-						for i in message:
-							self.arduino.write(i)
-			print("Error in formating package")
-			return False
-		print("Arduino isn't connected")
-		return False
-
+		try:
+			if self.arduino !=  None:
+				msg = bytes(bytearray([motor, dir, power]))
+				#print(msg)
+				self.arduino.write(msg)
+		except serial.SerialException:
+			self.arduino.close()
+			self.arduino = None
+			print("Arduino isn't connected")
+		
 	def close(self):
 		if self.arduino != None:
 			self.arduino.close()
@@ -55,3 +63,6 @@ class Base:
 def main():
 	base = Base()
 	base.loop()
+
+if __name__ == '__main__':
+	main()
