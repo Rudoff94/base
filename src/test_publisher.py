a6c8cb211ca0887d5ba4dad6ed9e4217b1e858cd
rudoff94 <rudovd94@gmail.com>
2020-03-29 16:50:52 +0000

    Added msg type BaseMotorControl

---
+++ src/test_publisher.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+from rpi_platform_ros.msg import BaseMotorControl
+from time import sleep
+
+def main():
+	rospy.init_node('Test_node')
+	pub = rospy.Publisher('base_motor_control',BaseMotorControl, queue_size = 5)
+	sleep(1)
+	msg = BaseMotorControl()
+	msg.motor = 0
+	msg.direction = 0
+
+	while not rospy.is_shutdown():
+		msg.power = 250
+		pub.publish(msg)
+		sleep(3)
+		msg.power = 0
+		pub.publish(msg)
+		sleep(3)
+
+if __name__ == '__main__':
+	main()
