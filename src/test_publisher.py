c92b947206dbedce91a818344594538a0e52afba
Ubuntu User <rudovd94@gmail.com>
2020-03-31 18:01:07 +0000

    Changed test_publisher

--- src/test_publisher.py
+++ src/test_publisher.py
@@ -8,16 +8,35 @@ def main():
 	pub = rospy.Publisher('base/motor_control',BaseMotorControl, queue_size = 5)
 	sleep(1)
 	msg = BaseMotorControl()
-	msg.motor = 0
-	msg.direction = 0
-
+	
 	while not rospy.is_shutdown():
-		msg.power = 20
+		msg.motor = 0
+		msg.direction = 0
+		msg.power = 50
 		pub.publish(msg)
-		sleep(3)
+		msg.motor = 1
+		msg.direction = 1
+		msg.power = 50
+		pub.publish(msg)
+		sleep(1)
+		msg.motor = 0
+		msg.direction = 1
+		msg.power = 50
+		pub.publish(msg)
+		msg.motor = 1
+		msg.direction = 0
+		msg.power = 50
+		pub.publish(msg)
+		sleep(1)
+		msg.motor = 0
+		msg.direction = 1
 		msg.power = 0
 		pub.publish(msg)
-		sleep(3)
+		msg.motor = 1
+		msg.direction = 1
+		msg.power = 0
+		pub.publish(msg)
+		sleep(2)
 
 if __name__ == '__main__':
 	main()
