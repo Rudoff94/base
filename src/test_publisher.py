67482e101656e6ca3c101d626eff67d71a4180db
Ubuntu User <rudovd94@gmail.com>
2020-04-04 14:16:38 +0000

    Added remote control

--- src/test_publisher.py
+++ src/test_publisher.py
@@ -2,13 +2,28 @@
 import rospy
 from rpi_platform_ros.msg import BaseMotorControl
 from time import sleep
+import socket
+import struct
+
+def
 
 def main():
 	rospy.init_node('Test_node')
+	#server init
+	host = '192.168.2.108'
+	port = 2002
+	server = socket.socket(AF_INET, socket.SOCK_STREAM)
+	server.bind((host, port))
+	server.listen(2)
+	rospy.loginfo('GAMEPAD_SERVER starting..')
+	#init publisher
 	pub = rospy.Publisher('base/motor_control',BaseMotorControl, queue_size = 5)
 	sleep(1)
 	msg = BaseMotorControl()
-	
+
+	while True:
+		
+'''
 	while not rospy.is_shutdown():
 		msg.motor = 0
 		msg.direction = 0
@@ -37,6 +52,6 @@ def main():
 		msg.power = 0
 		pub.publish(msg)
 		sleep(2)
-
+'''
 if __name__ == '__main__':
 	main()
